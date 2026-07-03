7fcdb3d296c185b2654a6d36ffc4ce8f6ec55f17
Tom <tommackinnon@outlook.com>
2026-07-03 17:24:07 -0500

    Add PWA support

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "habla-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./css/styles.css",
+        "./js/app.js",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
